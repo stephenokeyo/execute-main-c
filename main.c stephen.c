20caf0ca7dd7cd9176611ba868d1cba1e0dcbff4
stephenokeyo <90698968+stephenokeyo@users.noreply.github.com>
2021-11-23 13:47:10 +0300

    Add files via upload

---
+++ main.c stephen.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int pass;
+    label:
+          printf("change password\n");
+          scanf("%d," $pass);
+        {
+              printf("welcome\n");
+
+          }
+          if(pass<2001)//you can change it;
+            printf("wrong password\n re");
+             go =label;
+}
+          (if(pass>2001)
+          {
+              printf("wrong password\n re");
+              go to =lable;
+}
+
+
+
+()           (
+             printf(" wrong password\n re"));
+            go to lable;
+          }
+(
+           return 0;
+
+)
